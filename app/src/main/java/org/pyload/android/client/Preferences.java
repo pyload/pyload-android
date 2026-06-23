10da2ddee51be4532fcc626f786a82740757110d
GammaC0de <gammac0de@users.noreply.github.com>
2026-06-24 01:47:27 +0300

    reorganise server URL preference

--- app/src/main/java/org/pyload/android/client/Preferences.java
+++ app/src/main/java/org/pyload/android/client/Preferences.java
@@ -1,12 +1,17 @@
 package org.pyload.android.client;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.MenuItem;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceScreen;
 
-public class Preferences extends AppCompatActivity {
+public class Preferences extends AppCompatActivity implements PreferenceFragmentCompat.OnPreferenceStartScreenCallback {
     @Override
     protected void attachBaseContext(android.content.Context newBase) {
         android.content.SharedPreferences prefs = newBase.getSharedPreferences(newBase.getPackageName() + "_preferences", android.content.Context.MODE_PRIVATE);
@@ -30,22 +35,41 @@ public class Preferences extends AppCompatActivity {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
 
-        getSupportFragmentManager()
-                .beginTransaction()
-                .replace(R.id.preferences_container, new SettingsFragment())
-                .commit();
+        if (savedInstanceState == null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.preferences_container, new SettingsFragment())
+                    .commit();
+        }
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == android.R.id.home) {
-            finish();
+            if (getSupportFragmentManager().getBackStackEntryCount() > 0) {
+                getSupportFragmentManager().popBackStack();
+            } else {
+                finish();
+            }
             return true;
         }
         return super.onOptionsItemSelected(item);
     }
 
-    public static class SettingsFragment extends PreferenceFragmentCompat {
+    @Override
+    public boolean onPreferenceStartScreen(PreferenceFragmentCompat caller, PreferenceScreen pref) {
+        Fragment fragment = new SettingsFragment();
+        Bundle args = new Bundle();
+        args.putString(PreferenceFragmentCompat.ARG_PREFERENCE_ROOT, pref.getKey());
+        fragment.setArguments(args);
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.preferences_container, fragment)
+                .addToBackStack(null)
+                .commit();
+        return true;
+    }
+
+    public static class SettingsFragment extends PreferenceFragmentCompat implements SharedPreferences.OnSharedPreferenceChangeListener {
         @Override
         public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
             setPreferencesFromResource(R.xml.preferences, rootKey);
@@ -56,6 +80,46 @@ public class Preferences extends AppCompatActivity {
                     pyLoadApp.applyTheme((String) newValue);
                     return true;
                 });
+            }
+
+            updateUrlSummary();
+        }
+
+        @Override
+        public void onResume() {
+            super.onResume();
+            if (getPreferenceManager().getSharedPreferences() != null) {
+                getPreferenceManager().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+            }
+            updateUrlSummary();
+        }
+
+        @Override
+        public void onPause() {
+            super.onPause();
+            if (getPreferenceManager().getSharedPreferences() != null) {
+                getPreferenceManager().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
+            }
+        }
+
+        @Override
+        public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, @Nullable String key) {
+            if ("host".equals(key) || "port".equals(key) || "ssl".equals(key)) {
+                updateUrlSummary();
+            }
+        }
+
+        private void updateUrlSummary() {
+            Preference serverConnection = findPreference("server_connection");
+            if (serverConnection != null) {
+                SharedPreferences prefs = getPreferenceManager().getSharedPreferences();
+                if (prefs != null) {
+                    String host = prefs.getString("host", "");
+                    String port = prefs.getString("port", "8000");
+                    boolean ssl = prefs.getBoolean("ssl", false);
+                    String protocol = ssl ? "https://" : "http://";
+                    serverConnection.setSummary(protocol + host + ":" + port);
+                }
             }
         }
     }
